df58b46497a873fb9fb8aa8316a7de837b39531e
Victor Alves <victor.alvescosta@outlook.com>
2020-07-30 21:37:53 -0300

    Subindo projeto aula 1 Imersão Alura - Madflix

---
+++ src/components/Menu/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import Img from '../../assets/img/Madflix_logo.png';
+import './menu.css';
+import Button from '../Button/';
+// import ButtonLink from '../components/ButtonLink';
+
+function Menu() {
+    return (
+        <nav className="Menu">
+            <a href="/">
+                <img className= "Img" src ={Img} alt="MadFlix logo"/>
+            </a>
+            <Button as="a" className= "ButtonLink" href="/">
+                Novo Vídeo
+            </Button>
+
+        </nav>
+
+    );
+}
+
+export default Menu;
