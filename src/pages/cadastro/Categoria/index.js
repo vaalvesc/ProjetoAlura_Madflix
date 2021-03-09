079546285bded247306782056607d59509f2b481
Victor Alves <victor.alvescosta@outlook.com>
2021-03-08 23:53:15 -0300

    corrigindo arquivo index.html - link htpps

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -1,11 +1,16 @@
-import React from 'react';
+import React, { useState }from 'react';
 import { Link } from 'react-router-dom';
 import PageDefault from '../../../components/PageDefault';
 
 function CadastroCategoria() {
+
+let [NomeDaCategoria, setNomeDaCategoria] = useState('Filmes')
+
+console.log([NomeDaCategoria], NomeDaCategoria)
+
     return (
     <PageDefault>
-      <h1>Cadastro de Categoria</h1>
+      <h1>Cadastro de Categoria: {NomeDaCategoria}</h1>
 
       <form>
 
