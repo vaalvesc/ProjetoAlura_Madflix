7628b884c5aedcf883dc749f20692e584b96b906
Victor Alves <victor.alvescosta@outlook.com>
2021-03-10 00:54:48 -0300

    criando setValores, inserindo dados de nomecategoria,descricao,cor e cadastro

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -1,37 +1,115 @@
-import React, { useState }from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import PageDefault from '../../../components/PageDefault';
 
 function CadastroCategoria() {
 
-let [NomeDaCategoria, setNomeDaCategoria] = useState('Filmes')
+  const [categorias, setCategorias] = useState(['Teste']);
 
-console.log([NomeDaCategoria], NomeDaCategoria)
+  const dadosIniciais = {
+    nome: '',
+    descricao: '',
+    cor: '',
 
-    return (
+  }
+
+  const [NomeDaCategoria, setNomeDaCategoria] = useState(dadosIniciais)
+  //função set - usada para alterar o NomeDaCategoria
+  //console.log([NomeDaCategoria], NomeDaCategoria)
+
+  function setValores(chave, valor) {
+    setNomeDaCategoria({
+      ...NomeDaCategoria,
+    [chave]: valor,
+    }
+
+    )
+  }
+
+  function HandlChanger(informeDados) {
+    setValores(informeDados.target.getAttribute('name'), 
+    informeDados.target.value
+    );
+  }
+
+  return (
     <PageDefault>
-      <h1>Cadastro de Categoria: {NomeDaCategoria}</h1>
+      <h1>Cadastro de Categoria: {NomeDaCategoria.nome}</h1>
 
-      <form>
+      <form onSubmit={function handleSubmit(informeDados) {
+        informeDados.preventDefault();
+        setCategorias([
+          ...categorias,
+          NomeDaCategoria
+        ]);
 
-        <label>
-        Nome da Categoria:
-        <input type="text"
-        />
-        </label>
+        setNomeDaCategoria(dadosIniciais)
+      }}>
+        <div>
+
+          <label>
+            Nome da Categoria:
+        <input
+              type="text"
+              //mostrar o valor dentro do compo nome da categoria
+              value={NomeDaCategoria.nome}
+              //funçao criada para alterar dados do campo nome da categoria
+              name="nome"
+              onChange={HandlChanger}
+            />
+          </label>
+        </div>
+        <div>
+          <label>
+            Descrição:
+        <input
+              type="text"
+              //mostrar o valor dentro do compo nome da categoria
+              value={NomeDaCategoria.descricao}
+              //funçao criada para alterar dados do campo nome da categoria
+              name="descricao"
+              onChange={HandlChanger}
+            />
+          </label>
+        </div>
+        <div>
+          <label>
+            Cor:
+        <input
+              type="color"
+              //mostrar o valor dentro do compo nome da categoria
+              value={NomeDaCategoria.cor}
+              //funçao criada para alterar dados do campo nome da categoria
+              name="cor"
+              onChange={HandlChanger}
+            />
+          </label>
+        </div>
 
         <button>
           Cadastrar
         </button>
       </form>
 
+      <ul>
+        {categorias.map((categoria, indice) => {
+          return (
+            <li key={`${categoria}${indice}`}>
+              {categoria.indice}
+            </li>
+          )
+        })}
+
+      </ul>
+
+
       <Link to="/">
-      Ir para Home
+        Ir para Home
         </Link>
-      
+
     </PageDefault>
-    )
-    }
-  
+  )
+}
+
 
 export default CadastroCategoria;
